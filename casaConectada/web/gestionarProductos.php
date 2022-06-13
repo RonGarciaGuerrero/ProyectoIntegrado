07bfa95e4bb7ee67969f47b90450990630853494
RonGarciaGuerrero <ronald.garcia.arq@gmail.com>
2022-06-13 16:11:11 +0200

    añado favicon en todas las páginas, edito seccion servicios

--- casaConectada/web/gestionarProductos.php
+++ casaConectada/web/gestionarProductos.php
@@ -17,6 +17,7 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Casa Conectada Domotica</title>
+      <link rel="icon" type="image/x-icon" href="../img/miniLogo.PNG">
       <link rel="stylesheet" href="../css/styles.css" />
       <meta charset="UTF-8" />
       <script src="../scripts/jquery-3.6.0.js"></script>
